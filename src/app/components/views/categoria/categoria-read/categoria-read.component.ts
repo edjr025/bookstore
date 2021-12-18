53789b0a98e531cb2bff066f40725b09cc21e726
edjr025 <edvaldojr025@gmail.com>
2021-12-18 17:57:30 -0300

    deploy

---
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -0,0 +1,34 @@
+import { CategoriaService } from './../categoria.service';
+import { Component, OnInit } from '@angular/core';
+import { Categoria } from '../categoria.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-categoria-read',
+  templateUrl: './categoria-read.component.html',
+  styleUrls: ['./categoria-read.component.css']
+})
+export class CategoriaReadComponent implements OnInit {
+
+  categoria: Categoria[] = []
+
+  displayedColumns: string[] = ['id', 'nome', 'descricao', 'livro','acoes'];
+
+  constructor(private service: CategoriaService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+  
+ findAll(){
+   this.service.findAll().subscribe(resposta =>{
+     console.log(resposta)
+      this.categoria = resposta;
+   })
+ }
+
+  navegarParaCategoriaCreate(){
+    this.router.navigate(["categorias/create"])
+  }
+
+}
